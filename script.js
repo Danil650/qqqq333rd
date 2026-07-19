04e32f87ada6c4c44dc487c95c9c50da1fe1b8a9
qqkxry <v3pxrt@gmail.com>
2026-07-19 21:15:11 +0500

    h

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//orbits
 const centralOrbit = document.getElementById("central-orbit");
 const lowOrbit = document.getElementById("low-orbit");
 const midOrbit = document.getElementById("mid-orbit");
@@ -8,14 +9,30 @@ const highOrbit = document.getElementById("high-orbit");
 const maxOrbitSize = 64;
 const minOrbitSize = 8;
 
+// speed
+const speed = 30;
+
 function randomPosition() {
     const x = Math.ceil(Math.random() * window.innerWidth);
     const y = Math.ceil(Math.random() * window.innerHeight);    
 
-    console.log(`x is ${x}\ny is ${y}`);
+    // console.log(`x is ${x}\ny is ${y}`);
+    return [x, y];
 }
-
 function randomSize() {
     return Math.ceil(Math.random() * (maxOrbitSize - minOrbitSize)) + minOrbitSize;
 } 
 
+console.log(randomPosition()[0])
+
+centralOrbit.style.left = `${Math.ceil(window.innerWidth/2)}px`;
+centralOrbit.style.top = `${Math.ceil(window.innerHeight/2)}px`;
+
+lowOrbit.style.left = `${randomPosition()[0]}px`;
+lowOrbit.style.top = `${randomPosition()[1]}px`;
+
+midOrbit.style.left = `${randomPosition()[0]}px`;
+midOrbit.style.top = `${randomPosition()[1]}px`;
+
+highOrbit.style.left = `${randomPosition()[0]}px`;
+highOrbit.style.top = `${randomPosition()[1]}px`;
