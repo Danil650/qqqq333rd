98d56d0e6670da9e9a98db5c9f6dcb2198726377
qqkxry <v3pxrt@gmail.com>
2026-07-26 19:59:54 +0500

    vrashenya dobavleniy

--- script.js
+++ script.js
@@ -13,26 +13,32 @@ const minOrbitSize = 8;
 // speed
 const speed = 30;
 
+function randomAngle() {
+    return Math.ceil(Math.random() * 360);
+}
+
 function randomPosition() {
     const x = Math.ceil(Math.random() * window.innerWidth);
     const y = Math.ceil(Math.random() * window.innerHeight);    
 
-    // console.log(`x is ${x}\ny is ${y}`);
     return [x, y];
 }
 function randomSize() {
     return Math.ceil(Math.random() * (maxOrbitSize - minOrbitSize)) + minOrbitSize;
 } 
 
-console.log(randomPosition()[0]);
+// console.log(randomPosition()[0]);
 
 centralOrbit.style.left = `${Math.ceil(window.innerWidth/2)}px`;
 centralOrbit.style.top = `${Math.ceil(window.innerHeight/2)}px`;
 
 for (orbit of orbits) {
-    orbit.style.left = `${randomPosition()[0]}px`;
-    orbit.style.top = `${randomPosition()[1]}px`;
+    const [x, y] = randomPosition();
+    orbit.style.left = `${x}px`;
+    orbit.style.top = `${y}px`;
+
+    const originX = Math.ceil(window.innerWidth/2) - x;
+    const originY = Math.ceil(window.innerHeight/2) - y;
+
+    orbit.style.transformOrigin = `${originX}px ${originY}px`
 }
-
-
-
